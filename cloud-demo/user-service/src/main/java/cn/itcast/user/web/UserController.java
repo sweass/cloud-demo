836dcc7903e07e1536119781cb702061fa5d6941
Hikari <hikari@all.cn>
2023-08-09 15:40:02 +0800

    微服务初步部署

---
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,41 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.config.PatternProperties;
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
